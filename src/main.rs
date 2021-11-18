165979440c189a2733783fd8271ca3515c70f928
Glowstick // Robbie <90464994+Glowstick0017@users.noreply.github.com>
2021-11-18 12:28:16 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,42 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.channel_id.eq(&879561100260151356) {
-            if msg.content.to_uppercase() == "JED" {
-                let _ = msg.channel_id.say(&ctx.http, "what ".to_owned() + msg.author.name.as_str()).await;
+        // dont read bot messages
+        if !msg.author.bot {
+            // only responds in #bot-commands
+            if msg.channel_id.eq(&879561100260151356) {
+                
+                if msg.author.id == 417116039789281300 {
+                  let _ = msg.reply_ping(&ctx.http, "Screw You ".to_owned() + msg.author.name.as_str()).await;
+                  return;
+                }
+                if msg.content.to_uppercase().contains("JED") && msg.content.to_uppercase().contains("?") {
+                  let _ = msg.reply_ping(&ctx.http, "yeeyee ".to_owned() + msg.author.name.as_str()).await;
+                  return;
+                }
+                if msg.content.to_uppercase().contains("READY") {
+                  let _ = msg.reply_ping(&ctx.http, "let's get started ".to_owned() + msg.author.name.as_str()).await;
+                  return;
+                }
+                
+                if msg.content.to_uppercase().contains("HELLO THERE"){
+                  let _ = msg.reply_ping(&ctx.http,"General Kenobi").await;
+                }
+                if msg.content.to_uppercase() == msg.content {
+                  let _ = msg.reply_ping(&ctx.http,"I can hear voices at the back of the room...").await;
+                }
+                                  
+                // reads for any substring of 'jed' regardless of case
+                if msg.content.to_uppercase().contains(" JED ") ||
+                msg.content.to_uppercase().starts_with("JED ") ||
+                msg.content.to_uppercase().ends_with(" JED") ||
+                msg.content.to_uppercase() == "JED" {
+                    // reply with what followed by the name
+                    let _ = msg.reply_ping(&ctx.http, "what ".to_owned() + msg.author.name.as_str()).await;
+                }
+                else if msg.content.to_uppercase().contains("JED") {
+                    let _ = msg.reply_ping(&ctx.http, "it's jed").await;
+                }
             }
         }
     }
